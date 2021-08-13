f21c7fe3f225f12f8f00381a47826af6b02f03e6
Claire <windeya94@hotmail.com>
2021-08-13 16:59:18 +0800

    新增angular專案

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,7 @@ namespace API {
             services.AddDbContext<DataContext> (options => {
                 options.UseSqlServer (_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +47,8 @@ namespace API {
             app.UseHttpsRedirection ();
 
             app.UseRouting ();
+
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization ();
 
